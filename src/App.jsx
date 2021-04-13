92ac3c521889f3ba215601d071cabe4343d57727
xuejian <xuejianeie@163.com>
2021-04-13 13:34:30 +0800

    init: 配置antd按需加载，less及less-loader降级处理

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/" component={Admin}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+import { Form, Input, Button, Checkbox } from 'antd'
+
+const Login = () => {
+  return (
+    <div>
+      <span>111</span>
+      <Button type="primary">登录</Button>
+    </div>
+  )
+}
+
+export default Login
